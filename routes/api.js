97e44ce35dfbe8b80d389349cafe9a54d53bc8c7
Sandeep Acharya <i.am.sandeep.acharya@gmail.com>
2017-12-15 11:04:31 +0530

    no negative amount

--- routes/api.js
+++ routes/api.js
@@ -125,7 +125,13 @@ router.get('/question', requireLogin, (req, res) => {
 router.post('/bet', requireLogin, (req, res) => {
   const user_id = req.session.user.id;
   const question_id = req.body.question_id;
-  const amount = req.body.amount > 0 ? req.body.amount : 0;
+  const amount = req.body.amount;
+  if (amount < 0) {
+    return res.json({
+      error: true,
+      message: 'Do not be so negative',
+    });
+  }
   const opinion = req.body.opinion === 'true';
   console.log(opinion);
   User.findById(user_id, (err, user) => {
